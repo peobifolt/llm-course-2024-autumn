8bbd86295a9cd14aa4a2b54edff56842bf186484
unknown <ivchenkodmitry@mail.ru>
2024-10-23 16:05:57 +0300

    Add solution 02

--- Homework/02/solution.py
+++ Homework/02/solution.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn.functional as F
+import torchtune
 
 
 def compute_attention(queries, keys, values) -> torch.Tensor:
@@ -8,7 +9,11 @@ def compute_attention(queries, keys, values) -> torch.Tensor:
     keys- (BATCH_SIZE, SEQ_LENGTH, HIDDEN_DIM)
     values- (BATCH_SIZE, SEQ_LENGTH, HIDDEN_DIM)
     """
-    pass
+    batch_size, seq_length, dim_per_head = queries.size()
+    scores = torch.bmm(queries, keys.transpose(1, 2))
+    scores /= dim_per_head**0.5
+    attention_weights = F.softmax(scores, dim=-1)
+    return torch.bmm(attention_weights, values)
 
 
 def compute_multihead_attention(queries, keys, values, projection_matrix) -> torch.Tensor:
@@ -18,11 +23,23 @@ def compute_multihead_attention(queries, keys, values, projection_matrix) -> tor
     values- (BATCH_SIZE, N_HEADS, SEQ_LENGTH, DIM_PER_HEAD)
     projection_matrix- (N_HEADS*DIM_PER_HEAD, N_HEADS*DIM_PER_HEAD)
     """
-    pass
+    batch_size, n_heads, seq_length, dim_per_head = queries.shape
+    outputs = []
+    for i in range(n_heads):
+        head_queries = queries[:, i]  # (BATCH_SIZE, SEQ_LENGTH, DIM_PER_HEAD)
+        head_keys = keys[:, i]
+        head_values = values[:, i]
+        head_output = compute_attention(head_queries, head_keys, head_values)
+        outputs.append(head_output)
+
+    concatenated_output = torch.cat(outputs, dim=2)  # (BATCH_SIZE, SEQ_LENGTH, DIM_PER_HEAD * N_HEADS)
+    return torch.matmul(concatenated_output, projection_matrix)
 
 
 def compute_rotary_embeddings(x)-> torch.Tensor:
     """
     x- (BATCH_SIZE, SEQ_LENGTH, N_HEADS, DIM_PER_HEAD)
     """
-    pass
+    batch_size, seq_length, n_heads, dim_per_head = x.size()
+    rpe = torchtune.modules.RotaryPositionalEmbeddings(dim=dim_per_head, max_seq_len=seq_length)
+    return rpe(x)
